59f09b3be4ef1cb32b3c85f671b997dde05f9b38
Rob Chambers <rxc@creare.com>
2014-11-22 14:35:28 -0500

    more pypi massaging

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email=__author_email__,
     url=__url__,
     keywords=__keywords__,
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
