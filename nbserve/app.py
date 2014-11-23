4ae76deb6e95a915d372ef967dc33e1d77d4c30b
Rob Chambers <rdchambers@gmail.com>
2014-11-23 11:40:47 -0500

    Preinitialize a notebook runner via a thread so that the server still starts up fast, but the runner is ready on first page access.

--- nbserve/app.py
+++ nbserve/app.py
@@ -31,12 +31,23 @@ def render_index():
     return flask.render_template_string(template, notebooks=nbmanager.list_notebooks('.'))
 
 
+##
+# This thread initializes a notebook runner, so that it's
+# ready to go on first page access.
 runner = None
+import threading
+def make_notebook_runner():
+    global runner
+    from runipy.notebook_runner import NotebookRunner
+    runner = NotebookRunner(None)
+    print "Runner is ready."
+make_notebook_runner_thread = threading.Thread(target=make_notebook_runner)
+make_notebook_runner_thread.start()
 
 @flask_app.route('/<nbname>/')
 def render_page(nbname):
     global runner
-    from runipy.notebook_runner import NotebookRunner
+
     from IPython.nbconvert.exporters.html import HTMLExporter
 
     if not nbmanager.notebook_exists(nbname):
@@ -58,22 +69,23 @@ def render_page(nbname):
     for i in range(N_RUN_RETRIES):
         try:
             if runner is None:
-                runner = NotebookRunner(nb['content'])
-            else:
-                # Do as complete of a reset of the kernel as we can.
-                # Unfortunately, this doesn't really do a 'hard' reset
-                # of any modules...
-                class ResetCell(dict):
-                    """Simulates just enough of a notebook cell to get this
-                    '%reset -f' cell executed using the existing runipy
-                     machinery."""
-                    input = "%reset -f"
-                runner.run_cell(ResetCell())# shell.execute("%reset -f")#ResetCell())
-                runner.nb = nb['content']
+                make_notebook_runner_thread.join()
+
+            # Do as complete of a reset of the kernel as we can.
+            # Unfortunately, this doesn't really do a 'hard' reset
+            # of any modules...
+            class ResetCell(dict):
+                """Simulates just enough of a notebook cell to get this
+                '%reset -f' cell executed using the existing runipy
+                 machinery."""
+                input = "%reset -f"
+            runner.run_cell(ResetCell())
+            runner.nb = nb['content']
             print "Running notebook"
             runner.run_notebook(skip_exceptions=True)
             break
         except Empty as e:
+            print "WARNING: Empty bug happened."
             if i >= (N_RUN_RETRIES - 1):
                 raise
 
