2244dd1b73182cac5edf0b23bcb789a230c17e61
Rob Chambers <rdchambers@gmail.com>
2014-12-05 20:32:58 -0500

    fixing missing setup files

--- nbserve/meta.py
+++ nbserve/meta.py
@@ -1,5 +1,5 @@
 __progname__ = "nbserve"
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __description__ = """nbserve is a simple script that lets you serve
 iPython notebooks as read-only HTML files over the web. It has some
 default options that make the notebooks appropriate for generating
